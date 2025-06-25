b5e6de5c6f9139e90c5fe91a6e28850b64cc086f
frauAnastasiia <kulyani2003@outlook.com>
2025-06-26 01:27:59 +0200

    resolve conflicts

--- fleximove-backend/booking-service/src/main/java/com/instantmobility/booking/dto/BillingInfo.java
+++ fleximove-backend/booking-service/src/main/java/com/instantmobility/booking/dto/BillingInfo.java
@@ -1,18 +1,11 @@
 package com.instantmobility.booking.dto;
 
 import lombok.Data;
-<<<<<<< HEAD
-
-@Data
-=======
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
 @Data
-@Getter
 @NoArgsConstructor
->>>>>>> 4776a39 (add latest changes)
 public class BillingInfo {
     private String billingModel;
     private double rate;
--- fleximove-backend/booking-service/src/main/java/com/instantmobility/booking/dto/PaymentResponse.java
+++ fleximove-backend/booking-service/src/main/java/com/instantmobility/booking/dto/PaymentResponse.java
@@ -13,65 +13,7 @@ public class PaymentResponse {
     private UUID paymentId;
     private String paymentStatus;
     private String message;
-<<<<<<< HEAD
     private double amount;
     private LocalDateTime timeStamp;
     private String transactionId;
-=======
-
-    // Getters and setters
-    public UUID getPaymentId() {
-        return paymentId;
-    }
-
-    public void setPaymentId(UUID paymentId) {
-        this.paymentId = paymentId;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public String getTransactionId() {
-        return transactionId;
-    }
-
-    public void setTransactionId(String transactionId) {
-        this.transactionId = transactionId;
-    }
-
-
-    // Helper method to check if payment was successful
-    public boolean isSuccessful() {
-        return "COMPLETED".equals(status);
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String s) {
-    }
->>>>>>> 4776a39 (add latest changes)
 }
