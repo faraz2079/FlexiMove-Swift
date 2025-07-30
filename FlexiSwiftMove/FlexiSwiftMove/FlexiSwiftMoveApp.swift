1ab0e9b7d290de4e2622d4734e91fe3f4e050c35
faraz2079 <farazchahi2079@gmail.com>
2025-07-30 17:42:12 +0200

    swift part got added

---
+++ FlexiSwiftMove/FlexiSwiftMove/FlexiSwiftMoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexiSwiftMoveApp.swift
+//  FlexiSwiftMove
+//
+//  Created by Faraz on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct FlexiSwiftMoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
